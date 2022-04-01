7d15576f07ad6e2f6c3e8e25813a82a77e64de07
Vlad-Home <bilinvlad@gmail.com>
2022-04-01 20:47:42 +0300

    second stage

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,6 +4,9 @@ import argparse
 parser = argparse.ArgumentParser(description='Generate diff')
 parser.add_argument('files', metavar='first_file', type=ascii, nargs=1)
 parser.add_argument('files', metavar='second_file', type=ascii, nargs=1)
+parser.add_argument('-f','--format',
+                    help='set format of output')
+
 args = parser.parse_args()
 print(args.accumulate(args.files))
 
