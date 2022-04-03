4ff177d807955b051b5b48fb7a0007cedfaadd94
Vlad-Home <bilinvlad@gmail.com>
2022-04-03 20:19:45 +0300

    stage 6 fin

--- gendiff/scripts/generate_diff.py
+++ gendiff/scripts/generate_diff.py
@@ -5,33 +5,48 @@ import os
 
 
 def openfile(filewrapper):
-    if os.path.splitext(filewrapper.name)[1] in ['.yml','.yaml']:
+    if os.path.splitext(filewrapper.name)[1] in ['.yml', '.yaml']:
         file = yaml.load(filewrapper, Loader=yaml.Loader)
     if os.path.splitext(filewrapper.name)[1] in ['.json']:
         file = json.load(filewrapper)
-    return file 
+    return file
+
+
+def element_to_ans_list(key, sign, value, deep):
+    if isinstance(value, dict):
+        return (key, sign, diff_dict(value, value, deep + 1))
+    if isinstance(value, bool):
+        return (key, sign, str(value).lower())
+    if value is None:
+        return (key, sign, 'null')
+    return (key, sign, value)
 
 
 def generate_diff(pathtofile1, pathtofile2):
+    return diff_dict(openfile(pathtofile1), openfile(pathtofile2))
+
+
+def diff_dict(dict1, dict2, deep=0):
     ans = []
-    file1 = openfile(pathtofile1)
-    file2 = openfile(pathtofile2)
-    setfile1 = set(file1.keys())
-    setfile2 = set(file2.keys())
-    for item in setfile1 - setfile2:
-        ans.append((item, '-', file1[item]))
-    for item in setfile2 - setfile1:
-        ans.append((item, '+', file2[item]))
-    for item in setfile1 & setfile2:
-        if file1[item] == file2[item]:
-            ans.append((item, ' ', file1[item]))
+    setdict1 = set(dict1.keys())
+    setdict2 = set(dict2.keys())
+    for item in setdict1 - setdict2:
+        ans.append(element_to_ans_list(item, '-', dict1[item], deep))
+    for item in setdict2 - setdict1:
+        ans.append(element_to_ans_list(item, '+', dict2[item], deep))
+    for item in setdict1 & setdict2:
+        if dict1[item] == dict2[item]:
+            ans.append(element_to_ans_list(item, ' ', dict1[item], deep))
         else:
-            ans.append((item, '-', file1[item]))
-            ans.append((item, '+', file2[item]))
+            if isinstance(dict1[item], dict) and isinstance(dict2[item], dict):
+                ans.append((item, ' ', diff_dict(dict1[item], dict2[item], deep + 1))) # noqa E501
+            else:
+                ans.append(element_to_ans_list(item, '-', dict1[item], deep))
+                ans.append(element_to_ans_list(item, '+', dict2[item], deep))
     ans.sort(key=itemgetter(1), reverse=True)
     ans.sort(key=itemgetter(0))
     ansstring = '{\n'
     for item in ans:
-        ansstring = ansstring + "  {} {}: {}\n".format(item[1], item[0], item[2]) # noqa E501
-    ansstring = ansstring + '}'
+        ansstring = ansstring + " "*((deep*4)+2) + "{} {}: {}\n".format(item[1], item[0], item[2]) # noqa E501
+    ansstring = ansstring + " " * deep * 4 + '}'
     return ansstring
--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -3,20 +3,68 @@ from gendiff.scripts.gendiff import gendifference
 
 def test_two_fixtured_files():
     assert gendifference(['./gendiff/tests/fixtures/file1.json', './gendiff/tests/fixtures/file2.json']) == '''{
-  - follow: False
+  - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
   - timeout: 50
   + timeout: 20
-  + verbose: True
+  + verbose: true
 }'''
+
 
 def test_two_YAML_files():
     assert gendifference(['./gendiff/tests/fixtures/filepath1.yml', './gendiff/tests/fixtures/filepath2.yml']) == '''{
-  - follow: False
+  - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
   - timeout: 50
   + timeout: 20
-  + verbose: True
+  + verbose: true
+}'''
+
+
+def test_two_fractal_json_files():
+    assert gendifference(['./gendiff/tests/fixtures/file11.json', './gendiff/tests/fixtures/file12.json']) == '''{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
 }'''
