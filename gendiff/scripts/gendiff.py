47c6a976191313e292cc8a2341afd43bd4df6290
Vlad-Home <bilinvlad@gmail.com>
2022-04-03 11:19:44 +0300

    stage 3

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,14 +1,16 @@
 import argparse
+from gendiff.scripts.generate_diff import generate_diff
 
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('file1', metavar='first_file', type=argparse.FileType('r'), nargs='?')
+    parser.add_argument('file2', metavar='second_file', type=argparse.FileType('r'), nargs='?')
+    parser.add_argument('-f','--format',
+                        help='set format of output')
 
-parser = argparse.ArgumentParser(description='Generate diff')
-parser.add_argument('files', metavar='first_file', type=ascii, nargs=1)
-parser.add_argument('files', metavar='second_file', type=ascii, nargs=1)
-parser.add_argument('-f','--format',
-                    help='set format of output')
+    args = parser.parse_args()
+    generate_diff(args.file1,args.file2)
 
-args = parser.parse_args()
-print(args.accumulate(args.files))
-
-
+if __name__ == '__main__':
+    main()
 
---
+++ gendiff/scripts/generate_diff.py
@@ -0,0 +1,28 @@
+import json
+from operator import itemgetter
+
+def generate_diff(pathtofile1, pathtofile2): 
+    ans=[]
+    file1 = json.load(pathtofile1)
+    file2 = json.load(pathtofile2)
+    setfile1 = set(file1.keys())
+    setfile2 = set(file2.keys())
+    onlyfile1 = setfile1 - setfile2
+    onlyfile2 = setfile2 - setfile1
+    intersectionfiles = setfile1 & setfile2
+    for item in onlyfile1:
+        ans.append((item, '-', file1[item]))
+    for item in onlyfile2:
+        ans.append((item, '+', file2[item]))
+    for item in intersectionfiles:
+        if file1[item] == file2[item]:
+            ans.append((item, ' ', file1[item]))
+        else:
+            ans.append((item, '-', file1[item]))
+            ans.append((item, '+', file2[item]))
+    ans.sort(key=itemgetter(1),reverse=True)
+    ans.sort(key=itemgetter(0))
+    print('{')
+    for item in ans:
+        print("  {} {}: {}".format(item[1],item[0],item[2]))
+    print('}')
